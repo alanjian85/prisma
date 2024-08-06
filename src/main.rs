95a3d72ad2d3c4fa5412294c9592c3c2a8191bd3
Alan Jian <alanjian85@outlook.com>
2024-08-06 17:20:38 +0800

    feat: create window using winit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use std::error;
+use std::error::Error;
 
-use vulkano::{
-    instance::{Instance, InstanceCreateInfo},
-    VulkanLibrary,
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
 };
 
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let library = VulkanLibrary::new()?;
-    let instance = Instance::new(library, InstanceCreateInfo::default())?;
-    for physical_device in instance.enumerate_physical_devices()? {
-        println!("{}", physical_device.properties().device_name);
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
     }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::RedrawRequested => self.window.as_ref().unwrap().request_redraw(),
+            _ => (),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_loop = EventLoop::new()?;
+    let mut app = App::default();
+    event_loop.set_control_flow(ControlFlow::Wait);
+    event_loop.run_app(&mut app)?;
     Ok(())
 }
